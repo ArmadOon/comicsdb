71ea2c57fd84812e69bc889aeb2c692e21a464fc
Martin Pluhar <83763364+ArmadOon@users.noreply.github.com>
2024-11-24 21:48:31 +0100

    init of app

---
+++ src/main/kotlin/com/armadoon/comicsdb/ComicsdbApplication.kt
@@ -0,0 +1,11 @@
+package com.armadoon.comicsdb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ComicsdbApplication
+
+fun main(args: Array<String>) {
+	runApplication<ComicsdbApplication>(*args)
+}
